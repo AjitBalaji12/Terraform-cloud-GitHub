6579b46e02568ced64aec360d3ece66035670e63
AjitBalaji12 <111441791+AjitBalaji12@users.noreply.github.com>
2022-11-21 21:38:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
         #profile = "default"
 	}
 
-resource "aws_instance" "Ajitinfra" {
+resource "aws_instance" "Ajitinfra12" {
 	ami = "ami-0b0dcb5067f052a63" 
 	instance_type = "t2.nano"
 }
